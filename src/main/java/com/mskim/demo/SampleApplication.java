7512a00b01c1ea83a7c8bc7e9ce15144b9c8ca29
MSKIM\kmins <mskim@opennaru.com>
2025-03-05 10:33:09 +0900

    중복로그인방지 -> 기존 유저 로그아웃

--- src/main/java/com/mskim/demo/SampleApplication.java
+++ src/main/java/com/mskim/demo/SampleApplication.java
@@ -2,11 +2,12 @@ package com.mskim.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
